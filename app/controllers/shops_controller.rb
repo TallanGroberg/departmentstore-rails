075b49ed7ebc2f2d236d8e28dedfc1394c7cfc76
tallan groberg <tallan.taven@gmail.com>
2019-05-15 15:13:15 -0600

    add nav bar

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,4 @@
-class ShopController < ApplicationController
+class ShopsController < ApplicationController
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'shop#index'
-  resources :shop
+  root 'shops#index'
+  #
+  # get '/shops/:id/edit', to "shops#edit"
+  # get '/shops/:id', to "shops#show"
+  # get '/shops/new', to "shops#new"
+  # get '/shops', to "shops#index"
+  resources :shops
 
 end
