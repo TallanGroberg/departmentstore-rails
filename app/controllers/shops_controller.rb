6479c88bdb89c48ec07ed5a15af4fdc744c43c9e
tallan groberg <tallan.taven@gmail.com>
2019-05-15 16:20:58 -0600

    add create method

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,5 @@
 class ShopsController < ApplicationController
-# before_action :set_shop [:show, :edit, :update, :destroy]
+ # before_action :set_shop [:show ]
 
   def index
     @shops = Shop.all
@@ -10,8 +10,31 @@ class ShopsController < ApplicationController
 
   def new
     @shop = Shop.new
+    render partial: "form"
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+
+    if @shop.save
+      redirect_to shops_path(@shop)
+    else
+      render :new 
+
+    end
+
   end
 
   def edit
   end
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+      params.require(:shop).permit(:isle)
+  end
+
 end
