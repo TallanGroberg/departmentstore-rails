e5d359e39bb5da3f637743040b476ce94a10a8f8
tallan groberg <tallan.taven@gmail.com>
2019-05-15 15:56:58 -0600

    display shop at index

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,11 +1,15 @@
 class ShopsController < ApplicationController
+# before_action :set_shop [:show, :edit, :update, :destroy]
+
   def index
+    @shops = Shop.all
   end
 
   def show
   end
 
   def new
+    @shop = Shop.new
   end
 
   def edit
